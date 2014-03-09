8d42dab8552f0d694ec9f71c666203d900c87086
lwj <348808705@qq.com>
2014-03-09 20:24:08 +0800

    3

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -6,5 +6,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println("hello world!");
         System.out.println("haha");
+        System.out.println("hahahaha");
     }
 }
